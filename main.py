4652273eeed7814ee472ece34ff4ee30dbc969f0
Astarag Mohapatra <40949756+Athe-kunal@users.noreply.github.com>
2021-11-10 21:59:57 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import argparse
+from train_tune import train_cases
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Train and tune for the given stokc')
+    parser.add_argument('-n','--name',type=str,help='Name of the stock')
+    parser.add_argument('-t','--ticker',type=str,help='Ticker symbol')
+    args = parser.parse_args()
+
+    names = [args.name]
+    tickers = [args.ticker]
+    
+    train_cases(names,tickers)
